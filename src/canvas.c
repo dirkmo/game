bc0ac2e33ae0d7f4cb97148208227ccd637941b1
Dirk Möller <moeller.dirk80@gmail.com>
2019-03-03 21:29:23 +0100

    blit work

--- src/canvas.c
+++ src/canvas.c
@@ -14,17 +14,30 @@ uint32_t canvas_bytesize(const canvas_t *c) {
 }
 
 
-static void blitbyte( const uint8_t byte, uint8_t len, canvas_t *dst, uint32_t x, uint32_t y) {
-    uint8_t mask = 0xFFu >> (8-len);
+static void blitbyte( uint8_t byte, uint8_t len, canvas_t *dst, uint32_t x, uint32_t y) {
+    uint8_t src_mask = 0xFFu >> (8-len);
+    byte &= src_mask;
+    int dst_h_off = y % 8; // offset
+    uint32_t d1_addr = x + (y/8) * dst->w;
+    uint8_t *d1 = &dst->data[d1_addr];
+    int d1_count = 8 - dst_h_off; // how many pixels fit into d1 with regards to offset
+    uint8_t d1_mask = 0xFFu << dst_h_off;
+    *d1 &= ~d1_mask;
+    *d1 |= byte << dst_h_off;
 
-    printbits(mask);
+    if( d1_count < len ) {
+        uint32_t d2_addr = x + (y/8+1) * dst->w;
+        uint8_t *d2 = &dst->data[d2_addr];
+        int d2_count = len - d1_count;
+        uint8_t d2_mask = 0xFFu >> dst_h_off;
+        *d2 &= ~d2_mask;
+        *d2 |= byte >> d1_count;
+    }
 }
 
 void canvas_blit( const canvas_t *src, canvas_t *dst, uint32_t x, uint32_t y ) {
-    int h_off = y % 8; // offset
-
     for( int srcy = 0; srcy < src->h; srcy+=8 ) {
-        for( int i = 0; i < 1/*src->w*/; i++ ) {
+        for( int i = 0; i < src->w; i++ ) {
             int len = src->h - srcy;
             if( len > 8 ) {
                 len = 8;
--- src/game.c
+++ src/game.c
@@ -17,9 +17,9 @@ canvas_t screen = {
 };
 
 uint8_t spritedata[] = {
-    0x01, 0x00, 0x00, 0x01,
-    0x00, 0x00, 0x00, 0x00,
-    0x80, 0x00, 0x00, 0x80,
+    0xff, 0x01, 0x01, 0xff,
+    0xff, 0x00, 0x00, 0xff,
+    0xff, 0x80, 0x80, 0xff,
 };
 
 canvas_t sprite = {
@@ -27,16 +27,22 @@ canvas_t sprite = {
     .data = spritedata
 };
 
+canvas_t salma = {
+    .w = 128, .h = 64,
+    .data = (uint8_t*)pic_raw_salma
+};
+
 void game_init(void) {
     printf("Game machine started.\n");
     dem_enable();
     //dem_copy_raw(pic_raw_salma);
 
-    memset(screen.data, 0xFF, canvas_bytesize(&screen));
+    memset(screen.data, 0xff, canvas_bytesize(&screen));
     for( int i = 0; i < screen.h/8; i++ ) {
-        screen.data[i*screen.w] = 0xfe;
+        screen.data[i*screen.w] = 1;//0xfe;
     }
-    canvas_blit( &sprite, &screen, 2, 1);
+    //canvas_blit( &salma, &screen, 0, 0);
+    canvas_blit( &sprite, &screen, 2, 2);
 
     dem_copy_raw(screen.data);
 }
